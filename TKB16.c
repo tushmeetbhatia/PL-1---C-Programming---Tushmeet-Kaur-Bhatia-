b6b91f042e76139df2964c51ed4bf8b7ab291e49
tushmeetbhatia <tushmeetb@gmail.com>
2026-09-13 01:29:49 +0530

    Add files via upload

---
+++ TKB16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int num, i=1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("\nMultiplication table of %d:\n", num);
+    for(i=1; i<=10; i++)
+    {
+        printf("%d x %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
